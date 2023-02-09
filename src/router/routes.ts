d608a8be3f34c5292c349cd22cbda29d2b7b64e5
Gabriel Lima <gabriel.rslima10@gmail.com>
2023-02-09 14:13:51 -0300

    feat: adiciona rota para o resultado da pesquisa

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -1,6 +1,9 @@
 import type { RouteRecordRaw } from "vue-router";
+
 import Home from "../views/Home.vue";
+import SearchResult from "../views/SearchResult.vue";
 
 export const routes: RouteRecordRaw[] = [
   { path: "/", name: "Home", component: Home },
+  { path: "/result", name: "SearchResult", component: SearchResult },
 ];
---
+++ src/views/SearchResult.vue
@@ -0,0 +1,9 @@
+<script lang="ts" setup>
+  import { useRoute } from "vue-router";
+
+  const { query } = useRoute();
+</script>
+
+<template>
+  <h1>{{ query.q }}</h1>
+</template>
