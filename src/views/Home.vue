b93ff39af87ad6817e5b91f526a82f8e06c507a7
Gabriel Lima <gabriel.rslima10@gmail.com>
2023-02-09 15:49:41 -0300

    feat: adiciona contéudo para a rota inicial

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,3 +1,59 @@
+<script lang="ts" setup>
+  import SearchBar from "../components/SearchBar.vue";
+</script>
+
 <template>
-  <h1>Home!</h1>
+  <section class="home">
+    <SearchBar class="form">
+      <header class="header">
+        <img src="../assets/img/poke-ball.png" alt="" class="header__img" />
+        <h1 class="header__title">Buscador de Pokemon</h1>
+      </header>
+    </SearchBar>
+
+    <a
+      href="https://www.linkedin.com/in/gabriel-lima-860612236"
+      target="_blank"
+      rel="noreferrer"
+      class="link"
+    >
+      <small>&copy; Gabriel Lima</small>
+    </a>
+  </section>
 </template>
+
+<style lang="scss" scoped>
+  @import "../styles/main";
+  .home {
+    @include flex-center(column);
+    height: 100vh;
+    min-height: 300px;
+    gap: 12px;
+  }
+  .form {
+    @include flex-center(column);
+    width: 100%;
+    padding: 36px;
+    border-radius: 8px;
+    gap: 28px;
+    background-color: #12141d;
+  }
+  .header {
+    @include flex-center(row);
+    gap: 4px;
+  }
+  .header__img {
+    width: 36px;
+    height: 36px;
+  }
+  .header__title {
+    font-size: 1.4rem;
+    font-weight: 500;
+  }
+
+  @media screen and (min-width: $md-screen) {
+    .form {
+      max-width: 400px;
+    }
+  }
+</style>
