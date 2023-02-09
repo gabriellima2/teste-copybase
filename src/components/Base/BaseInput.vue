e59bf6bbdc6da109eb8ca19bd564b78978e281f6
Gabriel Lima <gabriel.rslima10@gmail.com>
2023-02-09 15:48:37 -0300

    feat: adiciona searchbar

---
+++ src/components/Base/BaseInput.vue
@@ -0,0 +1,34 @@
+<script lang="ts" setup>
+  type BaseInputProps = {
+    modelValue: string;
+    className?: string;
+    handleKeyDown?: (e: KeyboardEvent) => void;
+  };
+
+  type BaseInputEmits = {
+    (e: "update:modelValue", value: string): void;
+  };
+
+  const props = defineProps<BaseInputProps>();
+  const emits = defineEmits<BaseInputEmits>();
+</script>
+
+<template>
+  <input
+    :class="props.className || 'text-input'"
+    v-bind="$attrs"
+    :value="props.modelValue"
+    @keydown="props.handleKeyDown"
+    @input="
+      emits('update:modelValue', ($event.target as HTMLInputElement).value)
+    "
+  />
+</template>
+
+<style lang="scss" scoped>
+  @import "../../styles/main";
+  .text-input {
+    @include base-input();
+    height: 100%;
+  }
+</style>
---
+++ src/components/SearchBar.vue
@@ -0,0 +1,77 @@
+<script lang="ts" setup>
+  import { ref } from "vue";
+  import { useRouter } from "vue-router";
+
+  import BaseInput from "./Base/BaseInput.vue";
+
+  type SearchBarProps = {
+    className?: string;
+  };
+
+  const props = defineProps<SearchBarProps>();
+  const searchValue = ref("");
+  const router = useRouter();
+
+  function handleSubmit() {
+    if (!searchValue.value) return;
+    router.push({ path: "/result", query: { q: searchValue.value } });
+  }
+
+  function handleKeyDown({ key }: KeyboardEvent) {
+    if (key === "Enter") return handleSubmit();
+  }
+</script>
+
+<template>
+  <form :class="props.className">
+    <slot></slot>
+    <div class="container">
+      <label>
+        <BaseInput
+          v-model="searchValue"
+          title="Campo de pesquisa"
+          placeholder="Pesquisar"
+          :handle-key-down="handleKeyDown"
+          class="container__field"
+        />
+      </label>
+      <button
+        type="submit"
+        title="Pesquisar"
+        @click.prevent="handleSubmit"
+        class="container__search-button"
+      >
+        🔎
+      </button>
+    </div>
+  </form>
+</template>
+
+<style lang="scss" scoped>
+  @import "../styles/main";
+  .container {
+    @include flex-center(row);
+    @include base-input();
+    width: fit-content;
+    padding: 0px;
+  }
+  .container__field {
+    border: none;
+    padding: 10px;
+    border-radius: 0px;
+  }
+  .container__search-button {
+    text-decoration: none;
+    padding: 10px;
+    border-radius: 4px;
+    transition: background-color 0.2s ease;
+    background: none;
+    border: none;
+    cursor: pointer;
+
+    &:hover,
+    &:focus {
+      background-color: $detail-color;
+    }
+  }
+</style>
