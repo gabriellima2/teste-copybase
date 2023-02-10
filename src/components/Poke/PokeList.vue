ec81684077684c47d2116162b7f730e763d7bd53
Gabriel Lima <gabriel.rslima10@gmail.com>
2023-02-10 10:40:29 -0300

    refactor: melhora tratamento de resposta da requisição na listagem de pokemons

--- src/components/Poke/PokeList.vue
+++ src/components/Poke/PokeList.vue
@@ -1,6 +1,9 @@
 <script lang="ts" setup>
   import { useFetch } from "../../composables/useFetch";
 
+  import BaseLoading from "../Base/BaseLoading.vue";
+  import BaseError from "../Base/BaseError.vue";
+
   import { pokeServices } from "../../services/poke-service";
   import type { PreviewPokeDTO } from "../../dtos/preview-poke-dto";
 
@@ -15,13 +18,33 @@
 </script>
 
 <template>
-  <h1 v-if="isLoading">Carregando...</h1>
-  <h1 v-else-if="error">{{ error }}</h1>
-  <div v-else>
-    <ul>
-      <li v-for="pokemon of data" v-bind:key="pokemon.id">
-        <h1>{{ pokemon.name }}</h1>
-      </li>
-    </ul>
+  <div class="poke-list">
+    <BaseLoading v-if="isLoading" />
+    <div v-else-if="error" class="poke-list__error">
+      <BaseError
+        :message="error || 'Ocorreu um erro'"
+        :has-go-back-button="true"
+        :has-emoji="true"
+        :full-screen="true"
+      />
+    </div>
+    <div v-else>
+      <ul>
+        <li v-for="pokemon of data" v-bind:key="pokemon.id">
+          <h1>{{ pokemon.name }}</h1>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
+
+<style lang="scss" scoped>
+  @import "../../styles/main";
+  .poke-list {
+    @include center-full-screen(row);
+  }
+  .poke-list__error {
+    @include flex-center(column);
+    gap: 16px;
+  }
+</style>
